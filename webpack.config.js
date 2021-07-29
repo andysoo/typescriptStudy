20e9b261b4aa81e919f06d6a932fb4a1e722aa1e
andysoo <lxwtj00@163.com>
2021-07-29 15:43:31 +0800

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,62 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
-  mode: 'development',
+  mode: "development",
 
-  entry: path.resolve(__dirname, './src/index.ts'),
+  entry: path.resolve(__dirname, "./src/index.ts"),
 
   output: {
-    path: path.resolve(__dirname, './dist'),
-
-    filename: 'index.js'
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    //webpack自动生成的代码中不使用箭头函数，老旧浏览器不识别
+    environment: {
+      arrowFunction: false,
+    },
   },
 
   module: {
-    rules: [{
-      test: /\.ts$/,
-      loader: 'ts-loader'
-    }]
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      chrome: "88",
+                    },
+                    //模似js浏览器运行环境，支持老旧浏览器支持新语法
+                    corejs: "3",
+                    useBuiltIns: "usage",
+                  },
+                ],
+              ],
+            },
+          },
+          "ts-loader",
+        ],
+      },
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
-    })
+      template: "./index.html",
+    }),
   ],
 
+  //解析模块
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
-  }
-
-
-
+  },
 }
